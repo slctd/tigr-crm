df259941e288545771a508663832266ef761d7e0
Alexander Borovykh <immaculate.pine@gmail.com>
2012-07-16 23:26:10 +0400

    Edit, open and close buttons added to buttons panel. + Little bugfix when cancel button in events redirected to deals.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,6 +2,6 @@ module EventsHelper
   def link_to_change_status(event)
     text = t(".open")
     text = t(".close") if event.opened?
-    link_to text, event_change_status_path(event)
+    link_to text, event_change_status_path(event), class: (event.opened? ? "close-event" : "open")
   end
 end
