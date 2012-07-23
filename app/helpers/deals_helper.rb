e051b6df64ccebbc65465fd5e4b13f236025f299
Alexander Borovykh <immaculate.pine@gmail.com>
2012-07-23 22:14:50 +0400

    Locale-dependent currencies formatting added. Bug with smaller tasks icons in search results fixed.

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,2 +1,10 @@
+# encoding: utf-8
 module DealsHelper
+  def currency_to_symbol(abbreviation)
+    case abbreviation
+    when "RUR" then '<span class="rur">p<span>уб.</span></span>'.html_safe
+    when "USD" then "$"
+    else abbreviation
+    end
+  end
 end
