50693ecdbfb858f0cbeb8e61e21d312ba42e032d
Artur Trofimov <arturtr@gmail.com>
2013-05-31 02:01:35 +1100

    remove old oauth initializer

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-#OAUTH_CREDENTIALS = YAML.load_file(Rails.root.join("config", "oauth.yml"))[Rails.env]
-#
-#Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :twitter, OAUTH_CREDENTIALS['twitter']['app_id'], OAUTH_CREDENTIALS['twitter']['app_secret']
-#  provider :yandex, OAUTH_CREDENTIALS['yandex']['app_id'], OAUTH_CREDENTIALS['yandex']['app_secret']
-#  provider :google_oauth2, OAUTH_CREDENTIALS['google']['app_id'], OAUTH_CREDENTIALS['google']['app_secret'], { access_type: "offline", approval_prompt: "" }
-#  provider :vkontakte, OAUTH_CREDENTIALS['vkontakte']['app_id'], OAUTH_CREDENTIALS['vkontakte']['app_secret'], {scope: 'notify'}
-#end
