f4520ee3e2d95bce5c4e7168d534f114da811705
Artur Trofimov <arturtr@gmail.com>
2013-05-31 03:38:40 +1100

    remove hash.stringify_values

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Crm::Application.initialize!
-
-require File.expand_path('../../lib/hash', __FILE__)
--- lib/hash.rb
+++
@@ -1,8 +0,0 @@
-class Hash
-  def stringify_values
-    inject({}) do |options, (key, value)|
-      options[key] = value.to_s
-      options
-    end
-  end
-end
