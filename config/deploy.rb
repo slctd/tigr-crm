a34a901ce063aa8bced206a85d892dba8f0bb733
Artur Trofimov <arturtr@gmail.com>
2013-05-30 20:37:04 +1100

    fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,13 +13,13 @@ load "config/recipes/check"
 server config['CAP_SERVER'], :web, :app, :db, primary: true
 
 set :user, config['CAP_USER']
-set :application, "tigrcrm"
+set :application, config['CAP_APP_NAME']
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:tigrbz/tigr-crm.git"
+set :repository, config['CAP_GIT_REPO']
 set :branch, "master"
 
 default_run_options[:pty] = true
