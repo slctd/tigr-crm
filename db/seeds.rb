b60cd66178acbf99c9aab92fb44464251ee1b166
Artur Trofimov <arturtr@gmail.com>
2013-05-31 15:37:13 +1100

    add dictionary seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Type.destroy_all
+Stage.destroy_all
+Currency.destroy_all
+
+types = I18n.t 'types'
+
+types.each do |type|
+  type_class = "#{type[0]}_type".classify.constantize
+  type_values = type[1].stringify_keys.keys
+  type_values.each {|value| type_class.create name: value}
+end
+
+stages = I18n.t('stages').stringify_keys.keys
+
+stages.each do |name|
+  success_probability = name.scan(/\d+/).first
+  Stage.create name: name, success_probability: success_probability
+end
+
+Currency.create name: 'Рубль', abbreviation: 'RUR'
+Currency.create name: 'Доллар США', abbreviation: 'USD'
